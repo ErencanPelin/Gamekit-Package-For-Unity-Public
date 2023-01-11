a2ed6ddfc9d713522bc92ab039bdd42d93058d58
Eren <88778734+ErencanPelin@users.noreply.github.com>
2023-01-12 10:24:09 +1100

    Update Interactable.cs

---
+++ Runtime/Interactables/Interactable.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Gamekit.Runtime.Interactables
+{
+  public class Interactable : MonoBehaviour
+  {
+    //Scripts are hidden as the gamekit is a commercial project
+  }
+}
