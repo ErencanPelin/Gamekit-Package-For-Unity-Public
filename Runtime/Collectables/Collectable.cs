ac841a31b3075180ba2f92d150968b560b6099a5
Eren <88778734+ErencanPelin@users.noreply.github.com>
2023-01-13 09:25:35 +1100

    Update Collectable.cs

---
+++ Runtime/Collectables/Collectable.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Gamekit.Runtime.Collectables
+{
+  public class Collectable : MonoBehaviour, ICollectable
+  {
+    //Scripts are hidden as the gamekit is a commercial project
+  }
+}
