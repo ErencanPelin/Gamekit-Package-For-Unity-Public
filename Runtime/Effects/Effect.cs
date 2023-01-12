8813779285e2d4cb1f7b1d32323818c95c71e649
Eren <88778734+ErencanPelin@users.noreply.github.com>
2023-01-13 09:23:37 +1100

    Create Effect.cs

---
+++ Runtime/Effects/Effect.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Gamekit.Runtime.Effects
+{
+  public class Effect : MonoBehaviour, IEffect
+  {
+    //Scripts are hidden as the gamekit is a commercial project
+  }
+}
